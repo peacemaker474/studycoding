4a00615ab329577b2e9100a1e8670eb31ad0015d
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2021-06-04 00:21:35 +0900

    2021-06-02

--- Javascript/Challenge/4days.js
+++ Javascript/Challenge/4days.js
@@ -4,10 +4,11 @@ const color = ["#55efc4", "#fab1a0", "#ffeaa7"];
 body.style.backgroundColor = color[2];
 
 const handleResize = (e) => {
-    
-    if (window.innerWidth <= 700) {
+    const width = window.innerWidth;
+
+    if (0 < width && width <= 700) {
         body.style.backgroundColor = color[0]; // 초록
-    } else if (window.innerWidth <= 1000) {
+    } else if (701 < width && width <= 1200) {
         body.style.backgroundColor = color[2]; // 베이지
     } else {
         body.style.backgroundColor = color[1]; // 분홍
